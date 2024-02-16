9da82d1478b62b50a2680e772c18e94a7b7a777c
abc <abc.@gmail.com>
2024-02-16 15:17:04 +0530

    Third Commit

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ SortedList<int, Products> products = new SortedList<int, Products>();
 products.Add(1, new Products("Shampoo", 100, "Brand1", new DateTime(2023, 2, 16), new DateTime(2024, 2, 16)));
 products.Add(2, new Products("Facewash", 500, "Brand2", new DateTime(2023, 3, 16), new DateTime(2024, 3, 16)));
 products.Add(3, new Products("Serum", 600, "Brand3", new DateTime(2023, 4, 16), new DateTime(2024, 4, 16)));
-
+products.Add(4, new Products("ToothPaste", 1000, "Brand4", new DateTime(2023, 5, 16), new DateTime(2024, 5, 16)));
+products.Add(5, new Products("Soap", 1200, "Brand5", new DateTime(2023, 6, 16), new DateTime(2024, 6, 16)));
 
 // Display details
 Console.WriteLine("Index Key | PName    | Price | Brand | MfDate    | ExpDate");
